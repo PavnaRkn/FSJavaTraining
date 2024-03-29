1baf8c5f394c4bb9cfeb77c62a98d24436f2aa18
RKDigital <RKDigital@DESKTOP-269OAN9>
2024-03-29 16:35:54 +0530

    First commit

---
+++ FSJavaTraining/src/com/Javabasics/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.Javabasics.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ FSJavaTraining/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RKDigital
+ *
+ */
+module FSJavaTraining {
+}
