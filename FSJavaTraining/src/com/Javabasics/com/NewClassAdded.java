ed08640233af472f73c49bf2b4824becfdbe8292
Madhavi <madhavi.pulivarthi@gmail.com>
2024-03-29 23:35:02 +0530

    Committed new class

---
+++ FSJavaTraining/src/com/Javabasics/com/NewClassAdded.java
@@ -0,0 +1,10 @@
+package com.Javabasics.com;
+
+public class NewClassAdded {
+
+	public static void main(String[] args) {
+		System.out.println("Imported from git and added a new class to git");
+
+	}
+
+}
